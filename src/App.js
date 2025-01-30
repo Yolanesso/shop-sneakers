899a4405c769bea17c1125dd35a1108fe4813d78
Nikita Kopylov <138037124+Yolanesso@users.noreply.github.com>
2025-01-30 21:11:33 +0700

    Добавлена логика отображения карточек в зависимости от передаваемый свойств в объект

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import Card from './components/Card'
+import Card from './components/Card';
 import Drawer from './components/Drawer';
-import Header from './components/Header'
+import Header from './components/Header';
+
+const sneakersArray = [
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: 'img/1.jpg' },
+  { title: 'Мужские Кроссовки Nike Air Max 270', price: 15999, imageUrl: 'img/2.jpg' },
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: 'img/3.jpg' },
+  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: 'img/4.jpg' },
+];
 
 function App() {
-	return (
-		<div className='wrapper clear'>
-			<Drawer />
-
-			<Header />
-			<div className='content p-40'>
-				<div className='d-flex align-center justify-between mb-40'>
-					<h1>Все кроссовки</h1>
-					<div className='search-block d-flex'>
-						<img src='/img/search.svg' alt='search' />
-						<input type='text' placeholder='Поиск...' />
-					</div>
-				</div>
-				<div className='d-flex'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="wrapper clear">
+      <Drawer />
+      <Header />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="search" />
+            <input type="text" placeholder="Поиск..." />
+          </div>
+        </div>
+        <div className="d-flex">
+          {sneakersArray.map((obj) => (
+            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,22 @@
-function Card() {
+function Card(props) {
   return (
-    <div className='card'>
-						<div className='favorite'>
-							<img src='/img/empty-favorite.svg' alt='Unliked' />
-						</div>
-						<img width={133} height={112} src='img/1.jpg' alt='Sneakers' />
-						<h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-						<div className='d-flex justify-between align-center'>
-							<div className='d-flex flex-column'>
-								<span>Цена: </span>
-								<b>12 999 руб.</b>
-							</div>
-							<button className='button'>
-								<img width={11} height={11} src='/img/plus.svg' alt='plus' />
-							</button>
-						</div>
-					</div>
-  )
+    <div className="card">
+      <div className="favorite">
+        <img src="/img/empty-favorite.svg" alt="Unliked" />
+      </div>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена: </span>
+          <b>{props.price}</b>
+        </div>
+        <button className="button">
+          <img width={11} height={11} src="/img/plus.svg" alt="plus" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
