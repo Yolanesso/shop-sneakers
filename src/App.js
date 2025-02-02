413480be57dacd514545f7e874e3a6f49b30904a
Nikita Kopylov <138037124+Yolanesso@users.noreply.github.com>
2025-02-02 22:09:42 +0700

    Реализовано открытие корзины, а так-же визуальная возможность добавить пару кроссовок в избранное (useState). Также стили компонента Card были изолированы от общих.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Card from './components/Card';
+import React from 'react';
+import Card from './components/Card/';
 import Drawer from './components/Drawer';
 import Header from './components/Header';
 
@@ -6,14 +7,16 @@ const sneakersArray = [
   { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: 'img/1.jpg' },
   { title: 'Мужские Кроссовки Nike Air Max 270', price: 15999, imageUrl: 'img/2.jpg' },
   { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: 'img/3.jpg' },
-  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: 'img/4.jpg' },
+  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: 10000, imageUrl: 'img/4.jpg' },
 ];
 
 function App() {
+  const [cardOpened, setCartOpened] = React.useState(false);
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      {cardOpened ? <Drawer onClose={() => setCartOpened(false)} /> : null}
+      <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>Все кроссовки</h1>
@@ -24,7 +27,13 @@ function App() {
         </div>
         <div className="d-flex">
           {sneakersArray.map((obj) => (
-            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onPlus={() => console.log('Добавили в закладки')}
+              onFavorite={() => console.log('Добавили в избранное')}
+            />
           ))}
         </div>
       </div>
