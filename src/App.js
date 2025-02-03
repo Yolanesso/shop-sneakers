d0e541591e6543ab5dd0c5c47542fe3acf7ca114
Nikita Kopylov <138037124+Yolanesso@users.noreply.github.com>
2025-02-03 22:13:14 +0700

    Добавлена бэк (возможносмть извлекать данные с сервера) для карточек кроссовок

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Card from './components/Card/';
 import Drawer from './components/Drawer';
 import Header from './components/Header';
 
-const sneakersArray = [
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: 'img/1.jpg' },
-  { title: 'Мужские Кроссовки Nike Air Max 270', price: 15999, imageUrl: 'img/2.jpg' },
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: 'img/3.jpg' },
-  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: 10000, imageUrl: 'img/4.jpg' },
-];
-
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
   const [cardOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch('https://67a0c0455bcfff4fabe07a54.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
 
   return (
     <div className="wrapper clear">
-      {cardOpened ? <Drawer onClose={() => setCartOpened(false)} /> : null}
+      {cardOpened ? <Drawer items={cartItems} onClose={() => setCartOpened(false)} /> : null}
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
@@ -25,15 +30,9 @@ function App() {
             <input type="text" placeholder="Поиск..." />
           </div>
         </div>
-        <div className="d-flex">
-          {sneakersArray.map((obj) => (
-            <Card
-              title={obj.title}
-              price={obj.price}
-              imageUrl={obj.imageUrl}
-              onPlus={() => console.log('Добавили в закладки')}
-              onFavorite={() => console.log('Добавили в избранное')}
-            />
+        <div className="d-flex flex-wrap">
+          {items.map((obj) => (
+            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
           ))}
         </div>
       </div>
